17d23ad434345bccc3805cd23d27658776ca99a8
Agustin Levental <104096194+agustinlevental@users.noreply.github.com>
2023-09-04 12:32:22 -0300

    Update Track.jsx

--- src/Components/Track/Track.jsx
+++ src/Components/Track/Track.jsx
@@ -1,47 +1,35 @@
 import React from 'react';
-import track from "./track.css";
+import './track.css'; 
 
+function Track(props) {
+  const { track, onRemove, onAdd, isRemoval } = props;
 
-export class Track extends React.Component {
-
-
-    constructor (props) {
-      super(props);
-
-      this.addTrack = this.addTrack.bind(this)
-
-      this.removeTrack = this.removeTrack.bind(this)
-    }
-
-    removeTrack () {
-
-      this.props.onRemove (this.props.track)
-    
-      }
-
-    renderAction (){ 
-    if (this.props.isRemoval) 
-    {return <button className="Track-action" onClick={this.removeTrack}>-</button> }
-    else { return <button className="Track-action" onClick = {this.addTrack}>+</button>} }
-    
-
-    addTrack () {
-      this.props.onAdd (this.props.track);
-    }
-
-    render () { 
-    
-    return <div className="Track">
-    <div className="Track-information">
-      <h3> {this.props.track.name} </h3>
-     
-      <p>{this.props.track.artist} | {this.props.track.album}  </p>
-    </div>
-
-   
-   {this.renderAction ()}
-  
-  </div>
+  const removeTrack = () => {
+    onRemove(track);
   }
+
+  const addTrack = () => {
+    onAdd(track);
+  }
+
+  const renderAction = () => {
+    if (isRemoval) {
+      return <button className="Track-action" onClick={removeTrack}>-</button>
+    } else {
+      return <button className="Track-action" onClick={addTrack}>+</button>
+    }
+  }
+
+  return (
+    <div className="Track">
+      <div className="Track-information">
+        <h3>{track.name}</h3>
+        <p>{track.artist} | {track.album}</p>
+      </div>
+      {renderAction()}
+    </div>
+  );
 }
+
+export default Track;
 
